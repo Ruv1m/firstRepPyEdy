65b889f900e8c37c72f5fe6e3473454fc51d0ad3
Ruvym Pometko <ruvimpometko.job@gmail.com>
2025-03-05 16:04:12 +0200

    test commit

---
+++ firstProject.py
@@ -0,0 +1,11 @@
+# firstNum = int(input("Enter first number:" ))
+# secondNum = int(input("Enter second number: "))
+
+# print("sum:", firstNum + secondNum, "\n", "umnozhenie:", firstNum * secondNum, "\n", "delenie:", firstNum/secondNum, "\n", "kvadrat:", firstNum**secondNum, sep = "")
+
+num1 = 5
+num2 = 6
+
+num1 += 5
+
+print(num1)
